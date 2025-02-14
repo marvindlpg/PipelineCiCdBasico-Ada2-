3632a901fbe185231dfecf981ab74bec3f182809
unknown <marvin.dlpg@gmail.com>
2025-02-14 00:11:36 -0500

    Uppdate 1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req,res) =>{ 
-    res.send('Ada - Proceso de Selección Ing. DevOps OK'); 
+    res.send('Ada - Proceso de Selección Ing. DevOps Validando'); 
 }); 
 
 
